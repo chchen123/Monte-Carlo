360bcfd07f3c987a6e67278f61cc3284325a80d9
Christina Chen <39911405+chchen123@users.noreply.github.com>
2018-08-07 16:58:43 -0400

    Add files via upload

---
+++ NN_PCnoise.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+basicNN_real2real_pCnoise.py
+============================
+Testing a basic neural network on nuclear scattering data.
+Binary classification of protons and "everything else" (carbon and noise)
+Trains on real tests on real.
+"""
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+import numpy as np
+import scipy as sp
+from keras.utils import np_utils
+
+import sys
+sys.path.insert(0, '../modules/')
+#import metrics
+#metrics = metrics.BinaryMetrics()
+
+#fix random seed
+np.random.seed(7)
+epochs = 100
+validation_split = 0.25
+batch_size = 10
+
+#real data
+p_0130 = sp.sparse.load_npz('/home/chen/data/real/20x20x20/run_0130_pDisc.npz')
+C_0130 = sp.sparse.load_npz('/home/chen/data/real/20x20x20/run_0130_CDisc.npz')
+noise_0130 = sp.sparse.load_npz('/home/chen/data/real/20x20x20/run_0130_junkDisc.npz')
+p_0210 = sp.sparse.load_npz('/home/chen/data/real/20x20x20/run_0210_pDisc.npz')
+C_0210 = sp.sparse.load_npz('/home/chen/data/real/20x20x20/run_0210_CDisc.npz')
+noise_0210 = sp.sparse.load_npz('/home/chen/data/real/20x20x20/run_0210_junkDisc.npz')
+
+p_real = sp.sparse.vstack([p_0130, p_0210], format='csr')
+C_real = sp.sparse.vstack([C_0130, C_0210], format='csr')
+noise_real = sp.sparse.vstack([noise_0130, noise_0210], format='csr')
+
+#labels
+p_real_labels = np.zeros((p_real.shape[0],))
+C_real_labels = np.ones((C_real.shape[0],))
+noise_real_labels = np.ones((noise_real.shape[0],))
+
+#merging
+full_real_data = sp.sparse.vstack([p_real, C_real, noise_real], format='csr')
+full_real_labels_categorical = np.hstack((p_real_labels, C_real_labels, noise_real_labels))
+full_real_labels = np_utils.to_categorical(full_real_labels_categorical)
+print(full_real_labels)
+
+(train_data, test_data,
+labels_train, labels_test) = train_test_split(full_real_data,
+                                              full_real_labels,
+                                              test_size=validation_split,
+                                              random_state=42)
+
+#define model
+model = Sequential()
+model.add(Dense(128, input_dim=full_real_data.shape[1], activation='relu'))
+model.add(Dense(full_real_labels.shape[1], activation='sigmoid'))
+
+#compile the model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+#fit the model with a validation set split
+history = model.fit(train_data.todense(), labels_train,
+                    validation_data=(test_data.todense(), labels_test),
+                    epochs=epochs,
+                    batch_size=batch_size)
+
+print(history.history.keys())
+print("Maximum Validation Accuracy Reached: %.5f%%" % max(history.history['val_acc']))
+# summarize history for accuracy
+plt.figure(1)
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Single Layer NN Accuracy - p vs. C + junk')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+# plt.savefig('../plots/results/tilt/basicNN_sim_pCjunk_acc.pdf')
+# # summarize history for loss
+# plt.figure(2)
+# plt.plot(history.history['loss'])
+# plt.plot(history.history['val_loss'])
+# plt.title('Single Layer NN Loss - p vs. C + junk')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.savefig('../plots/results/tilt/basicNN_sim_pCjunk_loss.pdf')
+
+textfile = open('/home/chen/data/keras-results/pCnoise.txt', 'w')
+textfile.write('acc \n')
+textfile.write(str(history.history['acc']))
+textfile.write('\n')
+textfile.write('\nval_acc \n')
+textfile.write(str(history.history['val_acc']))
+textfile.write('\n')
+textfile.write('\nloss \n')
+textfile.write(str(history.history['loss']))
+textfile.write('\n')
+textfile.write('\nval_loss \n')
+textfile.write(str(history.history['val_loss']))
+textfile.write('\n')
+textfile.write('\nconfusion matrices \n')
+
+
+model_path = '/home/chen/data/models/pCnoise/'
+
+# serialize model to YAML
+model_yaml = model.to_yaml()
+with open(model_path + "basicNN_NOISE.yaml", "w") as yaml_file:
+    yaml_file.write(model_yaml)
+# serialize weights to HDF5
+model.save_weights(model_path + "basicNN_NOISE.h5")
+print("Saved model to disk")
---
+++ monte_carlo_proton.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  7 10:06:20 2018
+
+@author: chen
+"""
+
+import numpy as np
+import pytpc
+import yaml
+import h5py
+import argparse
+import logging
+import logging.config
+
+logger = logging.getLogger(__name__)
+
+parser = argparse.ArgumentParser(description='A script for the Monte Carlo Fitting')
+parser.add_argument('config', help='Path to a config file')
+parser.add_argument('input', help='path to an input event file')
+parser.add_argument('output', help='the output HDF5 file')
+parser.add_argument('proton', help='path to file containing proton-like evts')
+args = parser.parse_args()
+
+run_ID = args.input[-11:-3]
+
+with open(args.config, 'r') as f:
+    config = yaml.load(f)
+
+inFile = h5py.File(args.input, 'r')
+dataset_name = '/clean'
+evt_inFile = inFile[dataset_name]
+
+mcfitter = pytpc.fitting.MCFitter(config)
+chi_values = np.empty(shape=(0,0))
+evtids = np.empty(shape=(0,0))
+
+def event_iterator(input_evtid_set, output_evtid_set):
+    unprocessed_events = input_evtid_set - output_evtid_set
+    num_input_evts = len(input_evtid_set)
+    num_events_remaining = len(unprocessed_events)
+    num_events_finished = len(output_evtid_set)
+    if num_events_remaining == 0:
+        logger.warning('All events have already been processed.')
+        raise StopIteration()
+    elif num_events_finished > 0:
+        logger.info('Already processed %d events. Continuing from where we left off.', num_events_finished)
+
+    for i in unprocessed_events:
+        if i % 100 == 0:
+            logger.info('Processed %d / %d events', i, num_input_evts)
+        yield i
+    else:
+        raise StopIteration()
+        
+with open(args.proton, "r") as output:
+    for i in output:
+        evtids = np.append(evtids, [i])
+        
+with h5py.File(args.output, 'w') as outFile:
+    gp = outFile.require_group('monte carlo')
+    input_evtid_set = {int(k) for k in evtids}
+    num_input_evts = len(input_evtid_set)
+    logger.info('Input file contains %d events', num_input_evts)
+    output_evtid_set = {int(k) for k in gp}
+    
+    for evt_index in event_iterator(input_evtid_set, output_evtid_set):
+        try:
+            del_list = []
+            xyzs_h5 = evt_inFile[str(evt_index)]
+            xyzs = np.array(xyzs_h5)
+            for i in range(len(xyzs)):
+                if (xyzs[i,6]) > 75.0:
+                    del_list.append(i)
+            xyzs = np.delete(xyzs,del_list,axis=0)
+            xy = xyzs[:, 0:2]
+        except Exception:
+            logger.exception('Failed to read event with index %d from input', evt_index)
+            continue
+
+        try:
+            xy_C = np.ascontiguousarray(xy, dtype=np.double)
+            cx, cy = pytpc.cleaning.hough_circle(xy_C)
+        except Exception:
+            logger.exception('Cannot find the center of curvature for event with index %d', evt_index)
+            continue
+        
+        try:
+            uvw, (cu, cv) = mcfitter.preprocess(xyzs[:,0:5], center=(cx, cy), rotate_pads=False)
+        except Exception:
+            logger.exception('Failed to preprocess event with index %d', evt_index)
+            continue
+        
+        try:
+            mcres, minChis, all_params, good_param_idx = mcfitter.process_event(uvw, cu, cv, return_details=True)
+            if np.isnan(mcres['posChi2']) != True:
+                chi_values = np.append(chi_values, [mcres['posChi2']+mcres['enChi2']+mcres['vertChi2']])
+        except Exception:
+            logger.exception('Monte Carlo fitting failed for event with index %d', evt_index)
+            continue        
+
+        try:
+            dset = gp.create_dataset('{:d}'.format(evt_index), data=chi_values, compression='gzip')
+        except Exception:
+            logger.exception('Writing to HDF5 failed for event with index %d', evt_index)
+            continue
+        
+        chi_values = np.empty(shape=(0,0))
+
+print('MC fitting complete for '+run_ID)
