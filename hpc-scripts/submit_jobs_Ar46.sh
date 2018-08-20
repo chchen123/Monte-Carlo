31cc4ecdffb198af442b83504b0cf6f2ba462215
Christina Chen <39911405+chchen123@users.noreply.github.com>
2018-08-20 15:37:58 -0400

    Update and rename submit_jobs_120-137.sh to submit_jobs_Ar46.sh

---
+++ hpc-scripts/submit_jobs_Ar46.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#This Bash script calls the PBS job scripts - up to 15 jobs at a time
+
+for value in {105..119} #the numbers indicate the run numbers one wishes to fit
+do
+  RUN_NUM=$value
+  export RUN_NUM
+  qsub -v RUN_NUM monte_carlo_Ar46.sh
+done
