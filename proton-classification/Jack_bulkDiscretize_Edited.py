74d917ac3e196206c54113a8ded7f69431921a3e
Christina Chen <39911405+chchen123@users.noreply.github.com>
2018-11-30 15:53:14 -0500

    Update Jack_bulkDiscretize_Edited.py

--- proton-classification/Jack_bulkDiscretize_Edited.py
+++ proton-classification/Jack_bulkDiscretize_Edited.py
@@ -3,6 +3,7 @@
 """
 bulkDiscretize_p.py
 ===================
+Author: Jack Taylor
 Bulk discretization of hdf5 formatted point cloud data.
 """
 import sys
@@ -14,7 +15,7 @@ input_dir = '/home/chen/ar46/clean_events/clean_run_0131.h5'
 
 #Whether or not we want to sum charge and add noise during discretization
 CHARGE = True
-NOISE = False
+NOISE = False #Chen: changed noise to False
 
 data = dd.bulkDiscretize(input_dir, 20, 20, 20, CHARGE, NOISE)
 sp.sparse.save_npz('/home/chen/data/tilt/20x20x20/run_0131.npz', data)
